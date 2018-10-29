571d0a5e48cd075a51e9c2790beca4f4ad7d300d
Nicholas Daddona <nickd1957@comcast.net>
2018-10-29 16:29:06 -0400

    Update Schema Script
    Update schema script to include dummy data from postgresqlScript.sql.
    Dummy data will now be inserted into the current working schema for the
    database. The old schema file will have to be removed at a later date.

--- schema/data-mail.sql
+++ schema/data-mail.sql
@@ -20,97 +20,167 @@ DROP TABLE tasks_input CASCADE;
 DROP TABLE task_list CASCADE;
 
 -- All roles available to users of the system
-CREATE TABLE roles(
-    role_id   INT NOT NULL,
-    role_name VARCHAR(30),
-    PRIMARY KEY(role_id)
+CREATE TABLE roles (
+  role_id   INT NOT NULL,
+  role_name VARCHAR(30),
+  PRIMARY KEY (role_id)
 );
 
 -- All users of the system
-CREATE TABLE users(
-    user_id INT NOT NULL,
-    user_name VARCHAR(36) NOT NULL,
-    encrypted_password  VARCHAR(128) NOT NULL,
-    f_name VARCHAR(30),
-    l_name VARCHAR(30),
-    is_active boolean,
-    last_login timestamp,
-    last_logout timestamp,
-    PRIMARY KEY(user_id)
+CREATE TABLE users (
+  user_id            INT          NOT NULL,
+  user_name          VARCHAR(36)  NOT NULL,
+  encrypted_password VARCHAR(128) NOT NULL,
+  f_name             VARCHAR(30),
+  l_name             VARCHAR(30),
+  is_active          boolean,
+  last_login         timestamp,
+  last_logout        timestamp,
+  PRIMARY KEY (user_id)
 );
 
 -- Used by Spring Remember Me API to remember logins.
 CREATE TABLE Persistent_Logins (
-    username varchar(64) not null,
-    series varchar(64) not null,
-    token varchar(64) not null,
-    last_used timestamp not null,
-    PRIMARY KEY (series)
+  username  varchar(64) not null,
+  series    varchar(64) not null,
+  token     varchar(64) not null,
+  last_used timestamp   not null,
+  PRIMARY KEY (series)
 );
 
 -- Users listed with their roles (a user can have multiple)
-CREATE TABLE user_roles(
-    user_id INT NOT NULL,
-    role_id INT NOT NULL,
-    PRIMARY KEY(user_id, role_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(role_id) REFERENCES roles(role_id)
+CREATE TABLE user_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY (user_id, role_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
 -- Clients that are associated with data mail jobs
-CREATE TABLE clients(
-    client_id INT NOT NULL,
-    client_name VARCHAR(30),
-    addr VARCHAR(45),
-    PRIMARY KEY(client_id)
+CREATE TABLE clients (
+  client_id   INT NOT NULL,
+  client_name VARCHAR(30),
+  addr        VARCHAR(45),
+  PRIMARY KEY (client_id)
 );
 
 -- List of data mail jobs and their clients
-CREATE TABLE jobs(
-    job_id INT NOT NULL,
-    job_desc VARCHAR(60),
-    client_id INT,
-    PRIMARY KEY(job_id),
-    FOREIGN KEY(client_id) REFERENCES clients(client_id)
+CREATE TABLE jobs (
+  job_id    INT NOT NULL,
+  job_desc  VARCHAR(60),
+  client_id INT,
+  PRIMARY KEY (job_id),
+  FOREIGN KEY (client_id) REFERENCES clients (client_id)
 );
 
 -- List of all workflows and their descriptions
-CREATE TABLE workflows(
-    job_id INT NOT NULL,
-    wf_id INT NOT NULL,
-    wf_desc VARCHAR(60),
-    PRIMARY KEY(job_id, wf_id),
-    FOREIGN KEY(job_id) REFERENCES jobs(job_id)
+CREATE TABLE workflows (
+  job_id  INT NOT NULL,
+  wf_id   INT NOT NULL,
+  wf_desc VARCHAR(60),
+  PRIMARY KEY (job_id, wf_id),
+  FOREIGN KEY (job_id) REFERENCES jobs (job_id)
 );
 
 -- List of all available tasks and information associated with them
-CREATE TABLE task_list(
-    task_id INT NOT NULL,
-    acronym VARCHAR(6),
-    t_desc  VARCHAR(60),
-    is_billable BOOLEAN,
-    price money,
-    PRIMARY KEY(task_id)
+CREATE TABLE task_list (
+  task_id     INT NOT NULL,
+  acronym     VARCHAR(6),
+  t_desc      VARCHAR(60),
+  is_billable BOOLEAN,
+  price       money,
+  PRIMARY KEY (task_id)
 );
 
 -- List of all the recorded tasks input by employees
-CREATE TABLE tasks_input(
-    job_id INT NOT NULL,
-    wf_id INT NOT NULL,
-    task_id INT NOT NULL,
-    user_id INT NOT NULL,
-    task_desc VARCHAR(60),
-    t_input INT,
-    t_output INT,
-    t_dropped INT,
-    time_taken timestamp,
-    time_recorded timestamp,
-    PRIMARY KEY(job_id, wf_id, task_id),
-    -- this may have to be removed as job_id is also referenced via workflows table
-    FOREIGN KEY(job_id) REFERENCES jobs(job_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    -- this might have to be reworked as must reference both job_id and wf_id from workflows
-    FOREIGN KEY(job_id, wf_id) REFERENCES workflows(job_id, wf_id)
+CREATE TABLE tasks_input (
+  job_id        INT NOT NULL,
+  wf_id         INT NOT NULL,
+  task_id       INT NOT NULL,
+  user_id       INT NOT NULL,
+  task_desc     VARCHAR(60),
+  t_input       INT,
+  t_output      INT,
+  t_dropped     INT,
+  time_taken    INT,
+  time_recorded timestamp,
+  PRIMARY KEY (job_id, wf_id, task_id),
+  -- this may have to be removed as job_id is also referenced via workflows table
+  FOREIGN KEY (job_id) REFERENCES jobs (job_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  -- this might have to be reworked as must reference both job_id and wf_id from workflows
+  FOREIGN KEY (job_id, wf_id) REFERENCES workflows (job_id, wf_id)
 );
 
 -- Add any test data here
+
+-- Test User Data
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (7, 'Manager1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (6, 'Billing1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (5, 'DataProcessing1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (4, 'FileTransfer1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (3, 'Programmer1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (2, 'dbuser1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+insert into users (user_id, user_name, encrypted_password, is_active)
+values (1, 'dbadmin1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', TRUE);
+
+-- User Permissions
+insert into roles(role_id, role_name)
+values (1, 'ROLE_ADMIN');
+
+insert into roles(role_id, role_name)
+values (2, 'ROLE_USER');
+
+insert into roles(role_id, role_name)
+values (3, 'ROLE_Production_Programmer');
+
+insert into roles(role_id, role_name)
+values (4, 'ROLE_File_Transfer');
+
+insert into roles(role_id, role_name)
+values (5, 'ROLE_Data_Processing');
+
+insert into roles(role_id, role_name)
+values (6, 'ROLE_Billing');
+
+insert into roles(role_id, role_name)
+values (7, 'ROLE_Manager');
+
+
+-- Tie test users to user permissions
+insert into user_roles (USER_ID, ROLE_ID)
+values (1, 1);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (1, 2);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (2, 2);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (3, 3);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (4, 4);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (5, 5);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (6, 6);
+
+insert into user_roles (USER_ID, ROLE_ID)
+values (7, 7);
