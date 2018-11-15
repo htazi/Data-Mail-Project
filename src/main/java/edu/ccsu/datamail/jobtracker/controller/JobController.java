48f3e00edf5e0d0a062c0663c1ae9b254c756bfd
Nicholas Daddona <nickd1957@comcast.net>
2018-11-15 13:29:12 -0500

    Update Controllers to Work with new Services

--- src/main/java/edu/ccsu/datamail/jobtracker/controller/JobController.java
+++ src/main/java/edu/ccsu/datamail/jobtracker/controller/JobController.java
@@ -1,12 +1,14 @@
 package edu.ccsu.datamail.jobtracker.controller;
 
 import edu.ccsu.datamail.jobtracker.entity.job.Job;
-import edu.ccsu.datamail.jobtracker.entity.job.Workflow;
+import edu.ccsu.datamail.jobtracker.entity.job.JobNotFoundException;
 import edu.ccsu.datamail.jobtracker.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,10 +24,35 @@ public class JobController
         this.jobService = jobService;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/jobs/findjob")
+    public String getToJob()
+    {
+        return "job/findJob";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/jobs/findjob")
+    public String toGetToWf(@RequestParam("jobId") int jobId, Model model) throws JobNotFoundException
+    {
+        Job job = jobService.getJob(jobId);
+        if (job != null) {
+
+            int nextWorkflowId = jobService.findWFNumber(jobId);
+            model.addAttribute("job", job);
+            model.addAttribute("nextWorkflowId", nextWorkflowId);
+            return "workflow/createWorkflowPage";
+        }
+        else {
+            model.addAttribute("job", "No such a job exists");
+            return "job/findJob";
+        }
+    }
+
     @Controller
-    public class TestController {
-        @RequestMapping(value="/test")
-        public String showTestPage(HttpServletRequest request) {
+    public class TestController
+    {
+        @RequestMapping(value = "/test")
+        public String showTestPage(HttpServletRequest request)
+        {
             request.setAttribute("param1", "Ken");
             request.setAttribute("param2", "Yamanaka");
             return "forward:/test2";
@@ -33,9 +60,11 @@ public class JobController
     }
 
     @Controller
-    public class TestController2 {
-        @RequestMapping(value="/test2")
-        public String showTestPage(HttpServletRequest request) {
+    public class TestController2
+    {
+        @RequestMapping(value = "/test2")
+        public String showTestPage(HttpServletRequest request)
+        {
             String param1 = (String) request.getAttribute("param1");
             param1 = "Bakatare " + param1;
             request.setAttribute("param1", param1);
@@ -47,37 +76,13 @@ public class JobController
     }
 
     @Controller
-    public class TestController3 {
-        @RequestMapping(value="/test3")
-        public String showTestPage(@RequestParam String param1, @RequestParam String param2) {
+    public class TestController3
+    {
+        @RequestMapping(value = "/test3")
+        public String showTestPage(@RequestParam String param1, @RequestParam String param2)
+        {
             return "testPageView";
         }
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/jobs/findjob")
-    public String getTOJob()
-    {
-
-        return "job/findJob";
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/jobs/findjob")
-    public String toGetToWf(@RequestParam("jobId") int jobId, Model model)
-    {
-        Job job = jobService.getJob(jobId);
-        if(job!= null) {
-
-            int nextWorkflowId = jobService.findWFNumber(jobId);
-            model.addAttribute("job", job);
-            model.addAttribute("nextWorkflowId", nextWorkflowId);
-            return "workflow/createWorkflowPage";
-        }
-        else {
-            model.addAttribute("job", "No such a job exists");
-            return "job/findJob";
-        }
-
-
     }
 
 
