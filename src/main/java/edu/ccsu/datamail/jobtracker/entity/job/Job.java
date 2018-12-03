b3c4c20f7bd1a8e0e76690ac0010b55d23241f34
Nicholas Daddona <nickd1957@comcast.net>
2018-12-03 13:08:48 -0500

    Update Job JPA
    Update Job JPA to contain the database column data_mill_num as a field
    dataMillNumber. Update hashcode and equals to use this new field.

--- src/main/java/edu/ccsu/datamail/jobtracker/entity/job/Job.java
+++ src/main/java/edu/ccsu/datamail/jobtracker/entity/job/Job.java
@@ -20,6 +20,9 @@ public class Job implements Serializable
     @JoinColumn(name = "client_id", nullable = false)
     private Client client;
 
+    @Column(name = "data_mill_num")
+    private Integer dataMillNumber;
+
     /**
      * Default Constructor:
      */
@@ -31,15 +34,17 @@ public class Job implements Serializable
      * Alternate Constructor:
      * Creates a new Job with the associated description and client
      *
-     * @param jobId   the integer id of the job
-     * @param jobDesc the description of the job
-     * @param client  the client associated with the job
+     * @param jobId          the integer id of the job
+     * @param jobDesc        the description of the job
+     * @param client         the client associated with the job
+     * @param dataMillNumber the data-mill job number associated with the job
      */
-    public Job(Integer jobId, String jobDesc, Client client)
+    public Job(Integer jobId, String jobDesc, Client client, Integer dataMillNumber)
     {
         this.jobId = jobId;
         this.jobDesc = jobDesc;
         this.client = client;
+        this.dataMillNumber = dataMillNumber;
     }
 
     public int getJobId()
@@ -72,6 +77,16 @@ public class Job implements Serializable
         this.client = client;
     }
 
+    public Integer getDataMillNumber()
+    {
+        return dataMillNumber;
+    }
+
+    public void setDataMillNumber(Integer dataMillNumber)
+    {
+        this.dataMillNumber = dataMillNumber;
+    }
+
     /**
      * Determines if this object is equal to another object
      *
@@ -90,13 +105,16 @@ public class Job implements Serializable
 
         Job job = (Job) o;
 
-        if (jobId != job.jobId) {
+        if (jobId != null ? !jobId.equals(job.jobId) : job.jobId != null) {
             return false;
         }
         if (jobDesc != null ? !jobDesc.equals(job.jobDesc) : job.jobDesc != null) {
             return false;
         }
-        return client != null ? client.equals(job.client) : job.client == null;
+        if (client != null ? !client.equals(job.client) : job.client != null) {
+            return false;
+        }
+        return dataMillNumber != null ? dataMillNumber.equals(job.dataMillNumber) : job.dataMillNumber == null;
     }
 
     /**
@@ -107,9 +125,10 @@ public class Job implements Serializable
     @Override
     public int hashCode()
     {
-        int result = jobId;
+        int result = jobId != null ? jobId.hashCode() : 0;
         result = 31 * result + (jobDesc != null ? jobDesc.hashCode() : 0);
         result = 31 * result + (client != null ? client.hashCode() : 0);
+        result = 31 * result + (dataMillNumber != null ? dataMillNumber.hashCode() : 0);
         return result;
     }
 }
